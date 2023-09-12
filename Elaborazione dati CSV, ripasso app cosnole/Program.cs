c9ab71c463dcda8298ce0b3fd8827ad419c47567
Riccardo Camozzi <camozzi.riccardo06@gmail.com>
2023-09-12 21:09:45 +0200

    Aggiungere i file di progetto.

---
+++ Elaborazione dati CSV, ripasso app cosnole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elaborazione_dati_CSV__ripasso_app_cosnole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
